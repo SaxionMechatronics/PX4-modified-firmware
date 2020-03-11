d66cf30f1fa882815dacd7525613a7a3492eb857
Gerjen100 <gerjentermaat@hotmail.com>
2020-03-11 13:58:10 +0100

    Feature/gyro calib matrix (#1)
    * Added matrix math stuff
    
    * bugfix
    
    * removed unnecessary logging

--- src/lib/drivers/accelerometer/PX4Accelerometer.cpp
+++ src/lib/drivers/accelerometer/PX4Accelerometer.cpp
@@ -38,6 +38,7 @@
 
 using namespace time_literals;
 using matrix::Vector3f;
+using matrix::SquareMatrix;
 
 static inline int32_t sum(const int16_t samples[16], uint8_t len)
 {
@@ -90,14 +91,22 @@ int PX4Accelerometer::ioctl(cdev::file_t *filp, int cmd, unsigned long arg)
 	case ACCELIOCSSCALE: {
 			// Copy offsets and scale factors in
 			accel_calibration_s cal{};
-			// accel_calibration_misalign_s misalgn{}; TODO
-
 			memcpy(&cal, (accel_calibration_s *) arg, sizeof(cal));
 
 
 			_calibration_offset = Vector3f{cal.x_offset, cal.y_offset, cal.z_offset};
-			_calibration_scale = Vector3f{cal.x_scale, cal.y_scale, cal.z_scale};
-			_misalignment_scale = Vector3f{cal.x_misalign, cal.y_misalign, cal.z_misalign};
+			_calibration_scale = Vector3f{cal.x_scale, cal.y_scale, cal.z_scale};							//might be obsolete?
+			_misalignment_scale = Vector3f{cal.x_misalign, cal.y_misalign, cal.z_misalign}; 	//might be obsolete?
+
+			float misaling_data[9] = {1, 							0, 							0,
+															 cal.x_misalign, 	1, 							0,
+															 cal.y_misalign, 	cal.z_misalign, 1};
+		  _misalignment_matrix = SquareMatrix<float, 3>{misaling_data};
+
+			float scale_data[9] = {cal.x_scale,	0,					0,
+														0,						cal.y_scale,0,
+														0,						0,					cal.z_misalign};
+		  _scale_matrix = SquareMatrix<float, 3>{scale_data};
 
 		}
 
@@ -152,6 +161,9 @@ void PX4Accelerometer::update(hrt_abstime timestamp_sample, float x, float y, fl
 			_integrator_clipping++;
 		}
 	}
+
+	SquareMatrix<float, 3> D = _misalignment_matrix * _scale_matrix;
+	const Vector3f new_val_calibrated{inv(D) * ((raw * _scale) - _calibration_offset)};
 
 	// Apply range scale and the calibrating offset/scale
 	const Vector3f val_calibrated{((((raw * _scale) - _calibration_offset).emult(_calibration_scale))).emult(_misalignment_scale)};
