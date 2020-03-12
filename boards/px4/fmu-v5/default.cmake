3d58f4d375ed1c28bbf01668599c8338d66a4e06
Gerjen100 <Gerjentermaat@hotmail.com>
2020-03-12 15:18:48 +0100

    Added set up for csv file reader

--- boards/px4/fmu-v5/default.cmake
+++ boards/px4/fmu-v5/default.cmake
@@ -120,6 +120,7 @@ px4_add_board(
 		bottle_drop # OBC challenge
 		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
 		hello
+		imu_to_csv
 		hwtest # Hardware test
 		#matlab_csv_serial
 		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
